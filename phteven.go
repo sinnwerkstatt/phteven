20607c5281f48cc81366eb2149746bbf9de68ed2
Andreas Nüßlein <andreas@nuessle.in>
2019-08-19 10:14:44 +0200

    phteven 0.1

---
+++ phteven.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+func runBash(w http.ResponseWriter, r *http.Request) {
+	if _, err := os.Stat(".phteven.sh"); os.IsNotExist(err) {
+		const fileDoesNotExist = "File `.phteven.sh` does not exist"
+		log.Print(fileDoesNotExist)
+		fmt.Fprintf(w, fileDoesNotExist)
+		return
+	}
+
+	out, err := exec.Command("/bin/sh", ".phteven.sh").Output()
+	outString := string(out)
+	if err != nil {
+		log.Printf("[1] %s", outString)
+		http.Error(w, outString, http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("[0] %s", outString)
+	fmt.Fprintf(w, outString)
+}
+
+func main() {
+	port := "3000"
+	if len(os.Args) >= 2 {
+		port = os.Args[1]
+	}
+	portWithColon := fmt.Sprintf(":%s", port)
+
+	http.HandleFunc("/", runBash)
+	log.Printf("Phteven running. Serving on http://0.0.0.0%s ... ", portWithColon)
+	http.ListenAndServe(portWithColon, nil)
+}
